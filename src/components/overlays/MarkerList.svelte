45c71f12222d21cbc44ed00be3d1ed1099f1ee33
vulcangz <zhugd168@qq.com>
2021-03-01 21:54:07 +0800

    fix(overlays): fix `MarkerList` wrong file upload

--- src/components/overlays/MarkerList.svelte
+++ src/components/overlays/MarkerList.svelte
@@ -1,46 +1,52 @@
 <script>
-	import { getContext } from 'svelte'
-	import { contextKey } from './stores.js';
+	/**
+	 * 多个图像标注组件
+	 *
+	 * @component MarkerList
+	 * @example
+	 * <MarkerList markers={[ [74.438,39.006,1], [74.932,38.382,1], [75.402,37.879,1], [75.24,38.777,1] ]} />
+	 */
+
+	import { getContext } from 'svelte';
+	import { contextKey } from '../stores.js';
+	import Marker from './Marker.svelte';
 
 	const { getMap, getBdMap } = getContext(contextKey);
-  const map = getMap();
-  const bdmap = getBdMap();
+	const map = getMap();
+	const bdmap = getBdMap();
 
+	/**
+	 * 要在地图上展示的点坐标集合
+	 * @type {Array.<{lng: number, lat: number, label: string}}>}
+	 */
 	export let markers;
 
 	function clearMarkers() {
 		for (var i = 0; i < markers.length; i++) {
-				map.removeOverlay(markers[i]);
-				markers[i] = null;
+			map.removeOverlay(markers[i]);
+			markers[i] = null;
 		}
 		markers.length = 0;
 	}
 
-	if (document.createElement('canvas').getContext) {  // 判断当前浏览器是否支持绘制海量点
-			var points = [];  // 添加海量点数据
-			for (var i = 0; i < markers.length; i++) {
-        // console.log(markers[i]);
-        if (typeof markers[i].lng != "undefined" && typeof markers[i].lat != "undefined") {
-          points.push(new bdmap.Point(markers[i].lng, markers[i].lat));
-        } else if (typeof markers[i][0] != "undefined" && typeof markers[i][1] != "undefined") {
-          points.push(new bdmap.Point(markers[i][0], markers[i][1]));
-        } else {
-          console.log("Error: Point data format error!")
-        }
-			}
-			var options = {
-				size: BMAP_POINT_SIZE_SMALL,
-				shape: BMAP_POINT_SHAPE_STAR,
-				color: '#d340c3'
-			}
-			var pointCollection = new bdmap.PointCollection(points, options);  // 初始化PointCollection
-			pointCollection.addEventListener('click', function (e) {
-				alert('单击点的坐标为：' + e.point.lng + '。位于：' + e.point.lat);  // 监听点击事件
-			});
-			map.addOverlay(pointCollection);  // 添加Overlay
-  		//让所有点在视野范围内
-			map.setViewport(points);
-	} else {
-			alert('请在chrome、safari、IE8+以上浏览器查看');
+	// 判断当前浏览器是否支持绘制海量点
+	var points = []; // 添加海量点数据
+	for (var i = 0; i < markers.length; i++) {
+		// console.log(markers[i])
+		if (
+			typeof markers[i].lng != 'undefined' &&
+			typeof markers[i].lat != 'undefined'
+		) {
+			points.push(new bdmap.Point(markers[i].lng, markers[i].lat));
+		} else {
+			console.log('Error: Point data format error!');
+		}
 	}
+
+	// 让所有点在视野范围内
+	map.setViewport(points);
 </script>
+
+{#each markers as marker}
+	<Marker lng={marker.lng} lat={marker.lat} label={Marker.label} />
+{/each}
