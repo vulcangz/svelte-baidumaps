617c98567ca286b2cfd53803adaddfb68a6892dd
vulcangz <zhugd168@qq.com>
2021-03-01 21:02:22 +0800

    feat(overlays): add `PointCollection` component

--- src/components/components.module.js
+++ src/components/components.module.js
@@ -16,5 +16,6 @@ export { default as Walking } from './search/Walking.svelte';
 
 export { default as Circle } from './overlays/Circle.svelte';
 export { default as Label } from './overlays/Label.svelte';
+export { default as PointCollection } from './overlays/PointCollection.svelte';
 export { default as Polygon } from './overlays/Polygon.svelte';
 export { default as Polyline } from './overlays/Polyline.svelte';
